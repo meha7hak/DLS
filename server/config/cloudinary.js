ef88ac09ab3aa99e7222f2ef1476aedf18254a49
meha7hak <mehak.kotarya786@gmail.com>
2026-04-02 10:10:22 +0530

    profile picture upload fix

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: "./DLS.env" });
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
